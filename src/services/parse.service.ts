25293240b0f5f617c61d5b5925d6853020bcb5b0
Cesar <billypalacios616@gmail.com>
2026-05-26 16:14:57 -0400

    fix: corrigindo formato pendente para PENDENTE

--- src/services/parse.service.ts
+++ src/services/parse.service.ts
@@ -1,10 +1,8 @@
 import { prisma } from "../config/prisma";
 import { GoogleGenAI } from "@google/genai";
 
-export class ParserService{
-
-  async execute(message: string, userId: string){
-
+export class ParserService {
+  async execute(message: string, userId: string) {
     interface IItem {
       productName: string;
       quantity: number;
@@ -24,22 +22,23 @@ export class ParserService{
     const ai = new GoogleGenAI({});
     const response = await ai.models.generateContent({
       model: "gemini-3-flash-preview",
-      contents: "Extraia os dados da conversa de WhatsApp abaixo e retorne apenas o JSON no formato: {\"customerName\":\"Nome\",\"customerPhone\":\"Telefone\",\"address\":\"Endereço\",\"paymentMethod\":\"Forma de pagamento\",\"items\":[{\"productName\":\"Produto\",\"quantity\":0,\"unitPrice\":0.00,\"totalPrice\":0.00}],\"total\":0.00}. Regras: Use totalPrice = quantity * unitPrice; Se faltar telefone ou endereço, use 'N/A'; Retorne apenas o código, sem textos explicativos. Responda apenas com o JSON purificado. Não use blocos de código markdown. Conversa: " + message,
-    })
+      contents:
+        'Extraia os dados da conversa de WhatsApp abaixo e retorne apenas o JSON no formato: {"customerName":"Nome","customerPhone":"Telefone","address":"Endereço","paymentMethod":"Forma de pagamento","items":[{"productName":"Produto","quantity":0,"unitPrice":0.00,"totalPrice":0.00}],"total":0.00}. Regras: Use totalPrice = quantity * unitPrice; Se faltar telefone ou endereço, use \'N/A\'; Retorne apenas o código, sem textos explicativos. Responda apenas com o JSON purificado. Não use blocos de código markdown. Conversa: ' +
+        message,
+    });
 
     const parsedData: IParsedDataType = JSON.parse(response.text || "{}");
-
 
     const order = await prisma.order.create({
       data: {
         userId: userId,
-        customerName: parsedData.customerName,
-        customerPhone: parsedData.customerPhone,
-        address: parsedData.address,
-        paymentMethod: parsedData.paymentMethod,
-        status: "pending",
+        customerName: parsedData.customerName || "sem nome",
+        customerPhone: parsedData.customerPhone || "(00) 0 0000-0000",
+        address: parsedData.address || "sem endereço",
+        paymentMethod: parsedData.paymentMethod || "PIX",
+        status: "PENDENTE",
         total: parsedData.total,
-        source: "whatsapp",
+        source: "WHATSAPP",
         rawMessage: message,
 
         items: {
